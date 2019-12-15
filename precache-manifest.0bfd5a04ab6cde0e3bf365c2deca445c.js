217686a9f90db92544cec260316928f1e37a8db0
pbenmoser4 <pbenmoser4@gmail.com>
2019-12-15 14:42:14 +0100

    Updates

---
+++ precache-manifest.0bfd5a04ab6cde0e3bf365c2deca445c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548d1ff07770027f123ad5976ba8f044",
+    "url": "/pbenmoser4/index.html"
+  },
+  {
+    "revision": "52e46a81ba083cdca28a",
+    "url": "/pbenmoser4/static/js/2.40efbe20.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pbenmoser4/static/js/2.40efbe20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51c317676a18fdf6d363",
+    "url": "/pbenmoser4/static/js/main.58d59e5d.chunk.js"
+  },
+  {
+    "revision": "97440751229d80cb927c",
+    "url": "/pbenmoser4/static/js/runtime-main.29b98388.js"
+  },
+  {
+    "revision": "55dacd5be65f5d6e25a0f825dad946ad",
+    "url": "/pbenmoser4/static/media/pbm_icon.55dacd5b.png"
+  }
+]);
